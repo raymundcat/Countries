160841a39cd812e86ce435bac74537edf1f77119
John Raymund Catahay <raymund.cat@gmail.com>
2017-05-23 05:47:57 +0800

    unfinished filtering and imageloading management

---
+++ Countries/CountriesCollectionHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  CountriesCollectionHeaderView.h
+//  Countries
+//
+//  Created by John Raymund Catahay on 23/05/2017.
+//  Copyright © 2017 John Raymund Catahay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CountriesCollectionHeaderView : UIView
+
+@end
---
+++ Countries/CountriesCollectionHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  CountriesCollectionHeaderView.m
+//  Countries
+//
+//  Created by John Raymund Catahay on 23/05/2017.
+//  Copyright © 2017 John Raymund Catahay. All rights reserved.
+//
+
+#import "CountriesCollectionHeaderView.h"
+
+@implementation CountriesCollectionHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
